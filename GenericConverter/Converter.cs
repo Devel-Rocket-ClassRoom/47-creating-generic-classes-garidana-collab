1e3fedcc23c12bc02433d15d6b863674bfc8eb71
garidana-collab <garidana@gmail.com>
2026-03-12 20:36:14 +0900

    47 Generic Converter Done

--- GenericConverter/Converter.cs
+++ GenericConverter/Converter.cs
@@ -27,15 +27,19 @@ class Converter<TInput, TOutput>
         return _convertFunc(input);
     }
 
-    // 결과 배열 생성 
+    //결과 배열 생성
     public TOutput[] ConvertAll(TInput[] inputs)
     {
         // TOutput[] 은 배열이기 때문에 배열을 생성해서 각 반복문의 결과물을
         // 할당 해주어야 함으로 배열을 먼저 생성
-        TOutput[] outputs = new TOutputs
-        foreach (TInput input in inputs)
+        // TInput[] intputs의 길이 만큼 TOutput[] 배열 생성
+        TOutput[] results = new TOutput[inputs.Length];
+
+        for (int i = 0; i < inputs.Length; i++)
         {
-            return Convert(input);
+            // results 배열 변수에 Convert 함수로 input을 변환한 값을 할당
+            results[i] = Convert(inputs[i]);
         }
+        return results;
     }
 }
--- GenericConverter/Program.cs
+++ GenericConverter/Program.cs
@@ -5,8 +5,22 @@ using static System.Console;
 // README.md를 읽고 아래에 코드를 작성하세요.
 
 
-var conv = new Converter<string, int>( s => s.Length);
+var conv1 = new Converter<string, int>( s => s.Length);
+var conv2 = new Converter<int, string>(s => s.ToString());
+var conv3 = new Converter<double, int>(s => (int)s);
+
+string[] strings = new string[] {"Hello", "world", "C#"};
+int[] numbers = new int[] { 1, 2, 3 };
+double[] doubles = new double[] { 3.14, 1.2, 9.9 };
 
 WriteLine("=== 문자열 -> 길이 변환 ===");
-WriteLine($"Hello -> {conv.Convert("Hello")}");
-WriteLine()
+WriteLine($"Hello -> {conv1.Convert("Hello")}");
+WriteLine($"전체 변환: {string.Join(", ",(conv1.ConvertAll(strings)))}"); 
+WriteLine();
+WriteLine("=== 정수 -> 문자열 변환 ===");
+WriteLine($"1 -> {conv2.Convert(1)}번");
+WriteLine($"전체 변환: {string.Join("번, ", (conv2.ConvertAll(numbers)))}번");
+WriteLine();
+WriteLine("=== 실수 -> 문자열 변환 ===");
+WriteLine($"3.7 -> {conv3.Convert(3.7)}");
+WriteLine($"전체 변환: {string.Join(", ", (conv3.ConvertAll(doubles)))}");
